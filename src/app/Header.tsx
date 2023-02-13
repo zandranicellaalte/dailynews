0a34c42c12e431cbbd7e7bca1018fbdc8c13bf2b
Zandra Nicella Alte <85550797+zandranicellaalte@users.noreply.github.com>
2023-02-13 15:42:48 +0100

    Structured the layout

--- src/app/Header.tsx
+++ src/app/Header.tsx
@@ -1,26 +1,22 @@
 import Link from 'next/link';
 import NavCategoryLinks from './NavCategoryLinks';
 import SearchContainer from './SearchContainer';
+import LightDarkMode from './LightDarkMode';
 
 export default function Header() {
   return (
     <header>
       <div className="headerContainer">
-        {/* Heading */}
         <Link href="/" prefetch={false} className="Link">
           <h1 className="headerLogo">News</h1>
         </Link>
-
-        {/* Searchbar */}
         <div className="search">
           <SearchContainer />
         </div>
-
-        {/* Darkmode */}
-        <div className="darkmode">Darkmode {/* <DarkModeButton /> */}</div>
+        <div className="darkmode">
+          <LightDarkMode />
+        </div>
       </div>
-
-      {/* Navigation Link */}
       <NavCategoryLinks />
     </header>
   );
